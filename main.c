e7b64d5e28e5168f523e6dd71caf971e1b323e4b
sarunh10 <you@example.com>
2025-05-04 19:57:16 +0000

    Add script 0-preprocessor and test file

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
+ *Return : return 0 or 1
  */
-int main(void)
+void print_name(char *name, void (*f)(char *))
 {
-    return (0);
+if (name == NULL || f == NULL)
+return;
+f(name);
 }
