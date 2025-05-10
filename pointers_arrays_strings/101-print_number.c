4f3fc9f3300796aa52f21ff247056300c12b3266
sarunh10 <sarunh10@gmail.com>
2025-05-10 16:11:17 +0000

    task 101-print_number.c

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer using only _putchar
+* @n: the integer to print
+*/
+void print_number(int n)
+{
+unsigned int num;
+if (n < 0)
+{
+_putchar('-');
+num = -n;
+}
+else
+{
+num = n;
+}
+if (num / 10 != 0)
+{
+print_number(num / 10);
+}
+_putchar((num % 10) + '0');
+}
