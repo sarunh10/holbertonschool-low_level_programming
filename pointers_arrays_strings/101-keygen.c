323a1f91ec237deee19fd17ff4941e9cffa8a054
sarunh10 <sarunh10@gmail.com>
2025-05-10 12:28:23 +0000

    task 101-keygen.c

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,20 +3,18 @@
 #include <time.h>
 
 /**
-* main - generates a valid password for 101-crackme
+* main - generates a valid password for the program 101-crackme
 *
-* Return: Always 0
+* Return: Always 0.
 */
 int main(void)
 {
 int sum = 0;
 char c;
 srand(time(NULL));
-while (sum < 2772)
+while (sum < 2772 - 33)
 {
-c = rand() % 128;
-if ((sum + c) > 2772)
-break;
+c = (rand() % 94) + 33;
 sum += c;
 printf("%c", c);
 }
