8aa8860248807dea2f5384e69f3735ff4aea8680
sarunh10 <sarunh10@gmail.com>
2025-05-10 17:39:32 +0000

    task 102-magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -13,7 +13,6 @@ p = &n;
 /*
 * write your line of code here...
 * Remember:
-* - you are not allowed to use a
 * - you are not allowed to modify p
 * - only one statement
 * - you are not allowed to code anything else than this line of code
