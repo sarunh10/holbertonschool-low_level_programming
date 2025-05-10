32cd57c40a19647f2f79bd53dc6b6539ee4051b3
sarunh10 <sarunh10@gmail.com>
2025-05-10 17:25:30 +0000

    task 102-magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-
-
-
-
-
 /**
-* main - Entry point
-* Return: Always 0
+*main - modifies array using pointer trick to set a[2] = 98
+*Return: Always 0
 */
 int main(void)
 {
@@ -16,6 +11,14 @@ int a[5];
 int *p;
 a[2] = 1024;
 p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
