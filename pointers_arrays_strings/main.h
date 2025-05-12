04670f37d1cddf35284ab770c9b5590a9c2ffc4d
sarunh10 <sarunh10@gmail.com>
2025-05-12 16:01:53 +0000

    task 1-memcpy.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,29 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
-void print_number(int n);
-int _putchar(char c);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
+void swap_int(int *a, int *b);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 void reset_to_98(int *n);
 
