b90f31fda5ee7edb4acfe9ec5f648ed7e4c3101b
sarunh10 <sarunh10@gmail.com>
2025-05-14 23:27:33 +0000

    task 4-strpbrk.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,5 +5,5 @@ void swap_int(int *a, int *b);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 void reset_to_98(int *n);
-
+char *_strpbrk(char *s, char *accept);
 #endif
