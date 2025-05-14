8c8e6bebc0e12512fc46c0f6ad80037c0b4fc3d1
sarunh10 <sarunh10@gmail.com>
2025-05-14 23:45:42 +0000

    task  7-print_chessboard.c

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,4 +7,6 @@ char *_memset(char *s, char b, unsigned int n);
 void reset_to_98(int *n);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+int _putchar(char c);
+void print_chessboard(char (*a)[8]);
 #endif
