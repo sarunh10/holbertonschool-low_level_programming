8f7307c45c62fdf33b3996e2c0a607414476e05d
sarunh10 <sarunh10@gmail.com>
2025-05-25 01:20:40 +0000

    task dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structure that stores dog info
+* @name: dog's name
+* @age: dog's age
+* @owner: dog's owner
+* Description: Struct to store name, age, and owner of a dog.
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* DOG_H */
