23b117ee06ae8756f82fc4bb4b5fba4f452e7bcf
sarunh10 <sarunh10@gmail.com>
2025-05-25 00:34:44 +0000

    task 0-malloc_checked.c

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+/**
+* malloc_checked - Allocates memory using malloc
+* @b: The number of bytes to allocate
+* Return: A pointer to the allocated memory
+* If malloc fails, exit with status 98
+*/
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+ptr = malloc(b);
+if (ptr == NULL)
+exit(98);
+return (ptr);
+}
