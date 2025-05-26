0c0ffc842ffc2334136cf38098aec8e30de7acd4
sarunh10 <sarunh10@gmail.com>
2025-05-26 17:53:09 +0000

    task  4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,39 +1,53 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
 /**
-* new_dog - ينشئ كلب جديد ويعيد مؤشر إليه
-* @name: اسم الكلب
-* @age: عمر الكلب
-* @owner: صاحب الكلب
-*
-* Return: مؤشر إلى dog_t جديد، أو NULL إذا فشل
+* _strdup - duplicates a string using malloc
+* @s: the string to duplicate
+* Return: pointer to the duplicated string, or NULL on failure
+*/
+char *_strdup(char *s)
+{
+int i = 0, len = 0;
+char *dup;
+if (s == NULL)
+return (NULL);
+while (s[len] != '\0')
+len++;
+dup = malloc(sizeof(char) * (len + 1));
+if (dup == NULL)
+return (NULL);
+for (i = 0; i < len; i++)
+dup[i] = s[i];
+dup[i] = '\0';
+return (dup);
+}
+/**
+* new_dog - creates a new dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Return: pointer to the new dog_t, or NULL on failure
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *new;
-char *new_name, *new_owner;
-new = malloc(sizeof(dog_t));
-if (new == NULL)
+dog_t *dog;
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
 return (NULL);
-new_name = malloc(strlen(name) + 1);
-if (new_name == NULL)
+dog->name = _strdup(name);
+if (dog->name == NULL)
 {
-free(new);
+free(dog);
 return (NULL);
 }
-strcpy(new_name, name);
-new_owner = malloc(strlen(owner) + 1);
-if (new_owner == NULL)
+dog->owner = _strdup(owner);
+if (dog->owner == NULL)
 {
-free(new_name);
-free(new);
+free(dog->name);
+free(dog);
 return (NULL);
 }
-strcpy(new_owner, owner);
-new->name = new_name;
-new->age = age;
-new->owner = new_owner;
-return (new);
+dog->age = age;
+return (dog);
 }
