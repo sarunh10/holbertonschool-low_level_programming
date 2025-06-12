20e8ffc7be05f760b08f11e5064829b5ff6503dc
sarunh10 <sarunh10@gmail.com>
2025-06-12 20:09:24 +0000

    task 2-add_dnodeint.c

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -20,5 +20,5 @@ struct dlistint_s *next;
 /* Function prototype */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 #endif /* LISTS_H */
