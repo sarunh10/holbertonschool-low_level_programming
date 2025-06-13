dafa50d5b2c1119ef7e967f85579c3d5646e98e1
sarunh10 <sarunh10@gmail.com>
2025-06-13 03:19:04 +0000

    task 0-binary_to_uint.c

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* binary_to_uint - Converts a binary string to an unsigned int
+* @b: Pointer to a string of 0 and 1 chars
+*
+* Return: Converted number, or 0 if b is NULL or contains invalid chars
+*/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int result = 0;
+int i = 0;
+if (b == NULL)
+return (0);
+while (b[i] != '\0')
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+result = result * 2 + (b[i] - '0');
+i++;
+}
+return (result);
+}
